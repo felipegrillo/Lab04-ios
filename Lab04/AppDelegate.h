2e043e3ecacfb4aeb5795fe3cbd5a50e655cbd2f
lab04 <ieepo@felipe.local>
2015-02-05 11:28:44 -0600

    Initial Commit

---
+++ Lab04/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab04
+//
+//  Created by felipe on 05/02/15.
+//  Copyright (c) 2015 felipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
